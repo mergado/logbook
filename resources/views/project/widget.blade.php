115db536b6703522ff5091a09b3a990db760d582
hudecsamuel <hudecsamuel@gmail.com>
2016-05-27 11:14:49 +0200

    CHanged widget css
    Signed-off-by: hudecsamuel <hudecsamuel@gmail.com>

--- resources/views/project/widget.blade.php
+++ resources/views/project/widget.blade.php
@@ -9,66 +9,50 @@
     <title>Widget</title>
 
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
+    {{--<link rel="stylesheet" href="{!! asset('css/app.css') !!}">--}}
+    <link rel="stylesheet" href="{!! asset("css/simplegrid.css") !!}">
 
     <style type="text/css">
         ::-webkit-scrollbar {
             display: none;
         }
-        .paginator a,.paginator a:visited {
-            color: #009ba9;
-            text-decoration: none;
+
+        body {
+            background-color: hsla(0, 0%, 100%, .5);
+            margin-top: 0;
+            font-family: Arial, Helvetica, Verdana, Sans-serif;
+        }
+
+        .grid {
+            padding: 10px 10px;
         }
     </style>
 
 </head>
 
 <body>
-<div class="document-style">
-    <section id="content">
-        <div class="apps-widgets">
-            <section class="widget-wrapper">
-                <header>
-                    <div>
 
-                    </div>
-                </header>
-                <div class="widget cf">
-                    <div class="content">
-                        <table class="widget-table">
-                            <tbody>
-                            @if(count($logs) > 0)
-                                @foreach($logs as $log)
-                                    <tr title="{!! htmlentities($log->body) !!}">
-                                        <td class="left-column">{!! date('j. n. Y', strtotime($log->date)) !!}</td>
-                                        <td class="right-column ellipsis">
-                                            {!! $log->name !!}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @else
-                                <tr>
-                                    <td class="centered">
-                                        <b>{!! trans('log.no') !!}</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="centered" style="padding-top: 10px;">
-                                        <b>{!! trans('log.write_first') !!}</b>
-                                    </td>
-                                </tr>
-                            @endif
-                            </tbody>
-                        </table>
-
-                    </div>
-                    <div>
-                    </div>
+<div class="main">
+    <div class="grid">
+        @if(count($logs) > 0)
+            @foreach($logs as $log)
+                <div class="col-6-12" title="{!! htmlentities($log->body) !!}">
+                    {!! $log->name !!}
                 </div>
-            </section>
-        </div>
-    </section>
+                <div class="col-6-12" style="text-align: right">
+                    {!! date('j. n. Y', strtotime($log->date)) !!}
+                </div>
+            @endforeach
+        @else
+            <div class="col-1-1">
+                <b>{!! trans('log.no') !!}</b>
+            </div>
+            <div class="col-1-1">
+                <b>{!! trans('log.write_first') !!}</b>
+            </div>
+        @endif
 
+    </div>
 </div>
 
 </body>
