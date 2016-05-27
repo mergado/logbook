42f4a07dcbab6af44acae5bce844985cf19db7a5
hudecsamuel <hudecsamuel@gmail.com>
2016-05-27 11:39:25 +0200

    Different css for widget
    Signed-off-by: hudecsamuel <hudecsamuel@gmail.com>

--- resources/views/project/widget.blade.php
+++ resources/views/project/widget.blade.php
@@ -10,7 +10,28 @@
 
     <!-- Compiled and minified CSS -->
     {{--<link rel="stylesheet" href="{!! asset('css/app.css') !!}">--}}
-    <link rel="stylesheet" href="{!! asset("css/simplegrid.css") !!}">
+    {{--<link rel="stylesheet" href="{!! asset("css/simplegrid.css") !!}">--}}
+
+    {{--<style type="text/css">--}}
+        {{--::-webkit-scrollbar {--}}
+            {{--display: none;--}}
+        {{--}--}}
+
+        {{--body {--}}
+            {{--background-color: rgba(255,255,255,0.5);--}}
+            {{--margin-top: 0;--}}
+            {{--font-family: Arial, Helvetica, Verdana, Sans-serif;--}}
+            {{--font-size: 10pt;--}}
+        {{--}--}}
+
+        {{--.grid {--}}
+            {{--padding: 10px 10px;--}}
+        {{--}--}}
+
+        {{--div.center {--}}
+            {{--text-align: center;--}}
+        {{--}--}}
+    {{--</style>--}}
 
     <style type="text/css">
         ::-webkit-scrollbar {
@@ -18,47 +39,100 @@
         }
 
         body {
-            background-color: rgba(255,255,255,0.5);
+            background-color: hsla(0, 0%, 100%, .5);
             margin-top: 0;
             font-family: Arial, Helvetica, Verdana, Sans-serif;
-            font-size: 10pt;
-
         }
 
-        .grid {
-            padding: 10px 10px;
+        .widget {
+            display: block;
+            width: 100%;
+            height: 100vh;
+            overflow: hidden;
         }
 
-        div.center {
+        .widget-left, .widget-right {
+            height: 30px;
+            text-align: center;
+            color: #333;
+            float: left;
+        }
+
+        .widget-right {
+            padding-top: 5px;
+            width: 60%;
+        }
+
+        .widget-left {
+            width: 40%;
+        }
+
+        .widget-image {
+            height: 100%;
+            width: auto;
+            float: left;
+        }
+
+        .widget-right b, .widget-right p {
             text-align: center;
         }
+
+        .widget-right b {
+            font-size: 25pt;
+        }
+
+        .widget-right p {
+            font-size: 11pt;
+            margin-top: 2px;
+        }
     </style>
+
 
 </head>
 
 <body>
 
-<div class="main">
-    <div class="grid">
-        @if(count($logs) > 0)
-            @foreach($logs as $log)
-                <div class="col-6-12" title="{!! htmlentities($log->body) !!}">
-                    {!! $log->name !!}
-                </div>
-                <div class="col-6-12" style="text-align: right">
-                    {!! date('j. n. Y', strtotime($log->date)) !!}
-                </div>
-            @endforeach
-        @else
-            <div class="col-1-1 center">
-                <b>{!! trans('log.no') !!}</b>
-            </div>
-            <div class="col-1-1 center">
-                <b>{!! trans('log.write_first') !!}</b>
-            </div>
-        @endif
+{{--<div class="main">--}}
+    {{--<div class="grid">--}}
+        {{--@if(count($logs) > 0)--}}
+            {{--@foreach($logs as $log)--}}
+                {{--<div class="col-6-12" title="{!! htmlentities($log->body) !!}">--}}
+                    {{--{!! $log->name !!}--}}
+                {{--</div>--}}
+                {{--<div class="col-6-12" style="text-align: right">--}}
+                    {{--{!! date('j. n. Y', strtotime($log->date)) !!}--}}
+                {{--</div>--}}
+            {{--@endforeach--}}
+        {{--@else--}}
+            {{--<div class="col-1-1 center">--}}
+                {{--<b>{!! trans('log.no') !!}</b>--}}
+            {{--</div>--}}
+            {{--<div class="col-1-1 center">--}}
+                {{--<b>{!! trans('log.write_first') !!}</b>--}}
+            {{--</div>--}}
+        {{--@endif--}}
 
-    </div>
+    {{--</div>--}}
+{{--</div>--}}
+
+<div class="widget">
+    @if(count($logs) > 0)
+        @foreach($logs as $log)
+            <div class="widget-left" title="{!! htmlentities($log->body) !!}">
+                {!! $log->name !!}
+            </div>
+            <div class="widget-right">
+                {!! date('j. n. Y', strtotime($log->date)) !!}
+            </div>
+        @endforeach
+    @else
+        <div class="widget-left">
+        </div>
+        <div class="widget-right">
+            <b>Inactive</b>
+            <p><span style="color: indianred">0</span> active templates</p>
+        </div>
+    @endif
 </div>
 
 </body>
