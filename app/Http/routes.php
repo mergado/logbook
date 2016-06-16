f77bb0d648df3c9e40d89cd673f8bd0503eba104
hudecsamuel <hudecsamuel@gmail.com>
2016-06-16 12:13:38 +0200

    Changed routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,38 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+
+Route::post("/_mergado/hook/", "HookController@index");
 
 Route::group(['middleware' => ['web']], function () {
+
 	Route::get('oauth2', ['uses' => 'OAuthController@token', 'as' => 'token']);
 	Route::get('error', ['uses' => 'ErrorController@index', 'as' => 'error']);
 	Route::get('404page', ['uses' => 'ErrorController@notFound', 'as' => '404']);
 	Route::get('error/widget', ['uses' => 'ErrorController@widget', 'as' => 'errors.widget']);
 	Route::get('eshop/{eshop_id}/auth', ['uses' => 'OAuthController@auth', 'as' => 'auth']);
-});
 
-Route::post("/_mergado/hook/", "HookController@index");
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
+	Route::get('/logout', function(){
+		//invisible to normal user in mergado
+		Session::flush();
+	});
 
 	Route::group([
 			"prefix" => "eshop/{eshop_id}",
@@ -59,10 +41,6 @@ Route::group(['middleware' => ['web']], function () {
 		Route::get('/', 'WidgetController@eshopWidget');
 	});
 
-	Route::get('/logout', function(){
-		//invisible to normal user in mergado
-		Session::flush();
-	});
 });
 
 
